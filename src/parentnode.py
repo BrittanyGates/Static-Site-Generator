1e4c91796d1ba0072500030395d8cc95927080b1
Brittany Gates <bcgates82@gmail.com>
2026-03-12 19:14:57 -0500

    Created the ParentNode class and its unit tests.

---
+++ src/parentnode.py
@@ -0,0 +1,23 @@
+from htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError
+
+        if not self.children:
+            raise ValueError("HTML children missing.")
+
+        children_html = ""
+
+        for child in self.children:
+            children_html += child.to_html()
+
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} tag=\"{self.tag}\", children=\"{self.children}\", props=\"{self.props}\""
